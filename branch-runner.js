9329bc7f0ca6b63e23fd06ca913c3b79c18673d1
Jono <jono@foodnotblogs.com>
2016-01-04 17:10:02 -0800

    fixed issue that was caused by looking at the wrong git dir

--- branch-runner.js
+++ branch-runner.js
@@ -2,14 +2,8 @@ if (Meteor.isClient) {
 
   Meteor.startup(function () {
 
-    console.log('client start');
-
     setInterval(function () {
-
-      console.log("getting time");
-
       Meteor.call("getServerTime", function (error, result) {
-        console.log("got time");
         Session.set("time", result);
       });
     }, 2000);
@@ -44,16 +38,18 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
 
-  // config
-  // var serviceName = 'Test App';
-  // var baseDir = "/home/jono/ownCloud/lambdaCd/"
-  // var dockerfilesDir = baseDir + "/lcd-test-app";
-  var localGitDir = '/home/jono/files/lambdacd/lcd-test-app';
-  // var dockerfilesDir = localGitDir;
-  // end config
 
   var Future = Npm.require('fibers/future');
   spawn = Npm.require('child_process').spawn;
+
+  // config
+  var serviceName = 'Test App';
+  // var baseDir = "/home/jono/files/"
+  // var dockerfilesDir = baseDir + "/lcd-test-app";
+  var localGitDir = '/home/jono/files/lcd-test-app';
+  var dockerfilesDir = localGitDir;
+  // end config
+
 
   function getRemoteBranches() {
 
@@ -64,9 +60,12 @@ if (Meteor.isServer) {
 
     command.stdout.on('data', function (data) {
       var branchNames = (""+data).trim().split("\n");
-      console.log('remote branches: ' + branchNames);
 
       future.return(branchNames);
+    });
+
+    command.stderr.on('data', function (data) {
+      console.log("stderr: " + data);
     });
 
     return future.wait();
@@ -96,39 +95,6 @@ if (Meteor.isServer) {
         return _time;
     },
 
-    // getRemoteBranches: function () {
-    //
-    //   var future = new Future();
-    //
-    //   command = spawn('sh', ['-c',
-    //   "git --git-dir=/home/jono/files/lambdacd/lcd-test-app/.git branch --remote|sed 's/[^/]*\\///'"]);
-    //
-    //   command.stdout.on('data', function (data) {
-    //     var branchNames = (""+data).trim().split("\n");
-    //     // console.log('remote branches: ' + branchNames);
-    //
-    //     future.return(branchNames);
-    //   });
-    //
-    //   return future.wait();
-    // },
-
-    // getRunningBranches: function() {
-    //   var future = new Future();
-    //
-    //   command = spawn('sh', ['-c',
-    //   "docker ps --format '{{.Names}}\t{{.Ports}}' | grep _web_1 | sed 's/_web_1//' | sed 's/0.0.0.0://' | sed 's/->.*//'"]);
-    //
-    //   command.stdout.on('data', function (data) {
-    //     var branches = (""+data).trim().split("\n");
-    //     console.log('running branches: ' + branches);
-    //
-    //     future.return(branches);
-    //   });
-    //
-    //   return future.wait();
-    // },
-
     getBranches: function() {
       var allBranches = getRemoteBranches();
       var runingBranches = getRunningBranches();
@@ -139,6 +105,6 @@ if (Meteor.isServer) {
   });
 
   Meteor.startup(function () {
-
+    console.log('server start');
   });
 }
